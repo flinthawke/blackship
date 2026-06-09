374c4c89c8c4f9dcda79a3ad58d7a9b346856807
flinthawke <flinthawke@gmail.com>
2026-06-09 20:34:46 +0900

    Delete assets directory

--- assets/js/app.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const externalLinks = document.querySelectorAll('a[target="_blank"]');
-  externalLinks.forEach((link) => {
-    link.addEventListener('click', () => {
-      console.log('affiliate click:', link.href);
-    });
-  });
-});
